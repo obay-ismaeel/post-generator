b1b1055f2fabc40bdd321454ce512932baba9b90
Obay Ismaeel <obayhany@gmail.com>
2024-07-29 00:51:59 +0300

    Added whisper api

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
 from pydantic import BaseModel
 from llama_service import CreatePost
+import whisper
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import os
+from pathlib import Path
 
 app = FastAPI()
+
+model = whisper.load_model("base")
 
 class Item(BaseModel):
     script: str
@@ -23,3 +29,27 @@ async def generate_linkedin(item:Item):
 @app.post('/facebook')
 async def generate_linkedin(item:Item):
     return await CreatePost(item.script, item.link, "facebook")
+
+@app.post("/api/whisper")
+async def get_transcribe(file: UploadFile = File(...)):
+    # Get the current working directory
+    current_directory = Path.cwd()
+    # # Get the current working directory
+    # current_directory = os.getcwd()
+    
+    # Save the uploaded file to disk
+    file_path = f"{current_directory}{file.filename}"
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+
+    # Transcribe the audio using Whisper
+    result = model.transcribe(file_path)
+
+    # Remove the temporary file
+    os.remove(file_path)
+
+    # Extract the text from the transcription result
+    transcription_text = result.get("text", "")
+
+    # Return the transcription text as a JSON response
+    return JSONResponse(content={"text": transcription_text})
