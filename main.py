32689f6ebb4992c322ccff6fc2c3d9e604e2fd6e
Obay Ismaeel <obayhany@gmail.com>
2024-07-29 00:56:06 +0300

    Updated the route naming to match conventions

--- main.py
+++ main.py
@@ -14,42 +14,34 @@ class Item(BaseModel):
     script: str
     link: str
 
-@app.post('/blog')
+@app.post('/api/blog')
 async def generate_linkedin(item:Item):
     return await CreatePost(item.script, item.link, "blog")
 
-@app.post('/linkedin')
+@app.post('/api/linkedin')
 async def generate_linkedin(item:Item):
     return await CreatePost(item.script, item.link, "linkedin")
 
-@app.post('/twitter')
+@app.post('/api/twitter')
 async def generate_linkedin(item:Item):
     return await CreatePost(item.script, item.link, "twitter")
 
-@app.post('/facebook')
+@app.post('/api/facebook')
 async def generate_linkedin(item:Item):
     return await CreatePost(item.script, item.link, "facebook")
 
 @app.post("/api/whisper")
 async def get_transcribe(file: UploadFile = File(...)):
-    # Get the current working directory
     current_directory = Path.cwd()
-    # # Get the current working directory
-    # current_directory = os.getcwd()
     
-    # Save the uploaded file to disk
     file_path = f"{current_directory}{file.filename}"
     with open(file_path, "wb") as f:
         f.write(await file.read())
 
-    # Transcribe the audio using Whisper
     result = model.transcribe(file_path)
 
-    # Remove the temporary file
     os.remove(file_path)
 
-    # Extract the text from the transcription result
     transcription_text = result.get("text", "")
 
-    # Return the transcription text as a JSON response
     return JSONResponse(content={"text": transcription_text})
