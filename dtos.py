f67fcd39b615f06337826e7c2eb64376754a4c54
Obay Ismaeel <obayhany@gmail.com>
2024-08-24 19:53:38 +0300

    Add new dto

--- dtos.py
+++ dtos.py
@@ -1,8 +1,18 @@
-from pydantic import BaseModel
+from typing import Literal
+from pydantic import BaseModel, Field
 
+class QueryDto(BaseModel):
+    script: str
+    link: str
+    platform: Literal['linkedin', 'facebook', 'twitter', 'blog']
+    post_type: str | None = None
+    word_count: int | None = None
+    user_prompt: str | None = None
+    
 class Item(BaseModel):
     script: str
     link: str
+    platform: str | None = None
 
 class ScriptDto(BaseModel):
     script:str
